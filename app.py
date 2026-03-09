6abc0a111d2a852f39e330138bea86b9de49823e
abderrahmane-alloubane <dialmc9@gmail.com>
2026-03-09 15:05:28 +0000

    test actions

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "hello DevOps - Docker CI Pipeline!"
+	return "hello DevOps - Docker CI Pipeline! github!"
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000)
